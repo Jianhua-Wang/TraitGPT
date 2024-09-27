f562d9723a44da9c8df2f6007063dfc5302733cf
Jianhua-Wang <jianhua.mert@gmail.com>
2024-09-28 02:02:38 +0800

    Bump version: 0.0.9 → 0.0.10

--- traitgpt/__init__.py
+++ traitgpt/__init__.py
@@ -9,7 +9,7 @@ from .traitgpt import TraitGPT, VocabularyStore
 
 __author__ = """Jianhua Wang"""
 __email__ = "jianhua.mert@gmail.com"
-__version__ = "0.0.9"
+__version__ = "0.0.10"
 
 logging.basicConfig(
     level=logging.WARNING,
